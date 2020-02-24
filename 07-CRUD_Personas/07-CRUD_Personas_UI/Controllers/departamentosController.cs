fab9ecc5c39fc8a68d9a12088ae19f503a26e751
vperezlobato <55490747+vperezlobato@users.noreply.github.com>
2020-02-24 22:07:46 +0100

    subido

---
+++ 07-CRUD_Personas/07-CRUD_Personas_UI/Controllers/departamentosController.cs
@@ -0,0 +1,28 @@
+﻿using _07_CRUD_Personas_BL.Listados;
+using _07_CRUD_Personas_Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _07_CRUD_Personas_UI.Controllers
+{
+    public class departamentosController : ApiController
+    {
+        // GET: api/departamentos
+        clsListadoDepartamentosBL listado = new clsListadoDepartamentosBL();
+        public IEnumerable<clsDepartamento> Get()
+        {
+            return listado.listadoDepartamentos();
+        }
+
+        // GET: api/departamentos/5
+        public clsDepartamento Get(int id)
+        {
+            return listado.departamentoPorID(id);
+        }
+
+    }
+}
